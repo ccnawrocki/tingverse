e7eeac7262e7b6af49d828dc4a02c73bc81ec608
ccnawrocki <68296470+ccnawrocki@users.noreply.github.com>
2022-07-22 17:34:45 -0400

    Update deconvolcano.R

--- R/deconvolcano.R
+++ R/deconvolcano.R
@@ -32,7 +32,7 @@ deconvolcano <- function(normdata,metadata,feature,lab.1,lab.2,ID,n_cell_proxy,n
   metadata <- metadata[idx,c(ID,n_cell_proxy,feature)]
   colnames(metadata) <- c('Sample_ID',n_cell_proxy,feature)
   
-  immune_props <- as.data.frame(dec2$prop_of_all) %>% t() %>% 
+  immune_props <- as.data.frame(dec$prop_of_all) %>% t() %>% 
     cbind( 'Sample_ID' = rownames(immune_props)) %>% as.data.frame()
   
   metadata <- inner_join(metadata,immune_props,by='Sample_ID')
